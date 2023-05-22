ca2724f55f78011e4171155a101ba2fcb3a76991
Oleg Gera <hi@oleggera.com>
2023-05-22 20:24:31 +0100

    chore: hello world

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://notes.oleggera.com',
+  markdown: {
+    drafts: true,
+  },
+	integrations: [mdx({
+    drafts: true,
+  }), sitemap()],
 });
